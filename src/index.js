81a8b91108c70bbb1424b35f1b85eddf723896c6
Lucile Jones <jones.lucile@gmail.com>
2023-05-11 10:18:27 -0600

    initial react setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) =>  {
-    res.send('Hello World!');
-});
+// app.get('/', (req, res) =>  {
+//     res.send('Hello World!');
+// });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// });
+
+
+
+// const helloDiv = document.createElement("div");
+// helloDiv.innerHTML = "Hello from Javascript!";
+// document.body.append(helloDiv);
+
+// const sayHelloManyTimes = (times) =>
+//   new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+
+// const helloDiv = document.createElement("div");
+// helloDiv.innerHTML = sayHelloManyTimes(10).join("<br/>");
+// document.body.append(helloDiv);
+
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Hello from "./Hello";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Hello />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   target: 'node',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+// };
+
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -6,6 +19,31 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+  module: {
+    // exclude node_modules
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  // pass all js files through Babel
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  }
 };
