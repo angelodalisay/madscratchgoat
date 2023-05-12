b83390234d0632f457cdbf56e94948e8ab774662
Angelo <dalisay.angelo@gmail.com>
2023-05-12 12:48:27 -0600

    changes react fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development',
-//   target: 'node',
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-// };
-
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
