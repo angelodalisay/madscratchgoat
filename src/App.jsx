4f5a68231427b18719027c220054a5bcb767fa9b
lucilejones <99698725+lucilejones@users.noreply.github.com>
2023-05-24 17:13:42 -0600

    About (#6)
    * test push
    
    * About Component Update
    
    ---------
    
    Co-authored-by: Benjamin Vredevelt <bjv753@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar"
 import Hello from "./components/Hello";
+import About from "./components/About";
 
 export default function App() {
     return (
-        <>
+        <div id='app'>
             <Navbar />
+            <About />
             <Hello />
-        </>
+        </div>
 
 
     )
