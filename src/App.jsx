74716e9cd4edd4827e73318b36303bd4925f933a
Lucile Jones <jones.lucile@gmail.com>
2023-06-08 15:57:10 -0600

    sound cloud music section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar"
-import Hello from "./components/Hello";
+// import Hello from "./components/Hello";
 import About from "./components/About";
+import SoundCloud from "./components/SoundCloud";
 
 export default function App() {
     return (
         <div id='app'>
             <Navbar />
             <About />
-            <Hello />
+            <SoundCloud />
+            {/* <Hello /> */}
         </div>
 
 
---
+++ src/components/SoundCloud.jsx
@@ -0,0 +1,26 @@
+import ReactPlayer from "react-player/soundcloud";
+import './SoundCloud.css'
+
+
+
+export default function SoundCloud() {
+
+
+    return (
+        <div className="sound-cloud-section">
+            <p className="title">Music</p>
+            <div className="music-player-container">
+                <div id="player1" className="player">
+                    <ReactPlayer url="https://soundcloud.com/mad-scratch-goat/house-mix-2020&amp;color=01A0EB&show_user=false" width="100%" height="100%"/>
+                </div>
+                <div id="player2" className="player">
+                    <ReactPlayer url="https://soundcloud.com/mad-scratch-goat/house-to-edm&amp;color=01A0EB&show_user=false" width="100%" height="100%"/>
+                </div>
+                <div id="player3" className="player">
+                    <ReactPlayer url="https://soundcloud.com/mad-scratch-goat/chill-house-1&amp;color=01A0EB&show_user=false" width="100%" height="100%"/>
+                </div>
+            </div>
+        </div>
+
+    )
+}
