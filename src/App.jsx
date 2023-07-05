cb0888f29948053ab274d29e1ddd978f3b17fa30
Lucile Jones <jones.lucile@gmail.com>
2023-07-05 13:45:25 -0600

    added social media sectino and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Navbar from "./components/Navbar"
 // import Hello from "./components/Hello";
 import About from "./components/About";
 import SoundCloud from "./components/SoundCloud";
+import SocialMedia from "./components/SocialMedia";
+import Footer from "./components/Footer";
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <About />
+            <SocialMedia />
             <SoundCloud />
             {/* <Hello /> */}
+            <Footer />
         </div>
 
 
