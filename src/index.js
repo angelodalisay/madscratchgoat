937711990f3c257e23e4f4162a0b8954c462a4f1
Natalie Taylor <sisterchicago@gmail.com>
2023-07-13 14:51:47 -0600

    adjusted css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import './css/index.css'
 import './css/about.css'
+import './css/carousel.css'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
