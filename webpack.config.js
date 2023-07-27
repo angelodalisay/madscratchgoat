82e86161c920cdcb83eea356a35278665d171027
lucilejones <99698725+lucilejones@users.noreply.github.com>
2023-07-26 21:36:56 -0600

    Carousel (#12)
    * work please
    
    * created main carousel image banner
    
    * adjusted css
    
    * adjusted carousel img size
    
    ---------
    
    Co-authored-by: Natalie Taylor <sisterchicago@gmail.com>
    Co-authored-by: Angelo <dalisay.angelo@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader"]
       },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       {
