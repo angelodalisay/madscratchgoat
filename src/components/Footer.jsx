7033b1cbe4c2b028fcfbe753723e599020dc140a
Angelo Dalisay <dalisay.angelo@gmail.com>
2023-09-21 14:02:57 -0600

    Social media links (#23)
    * updates on social media section
    
    * still needs more updates
    
    * changed out social media images to font awesome
    
    * additional alignment cleanup
    
    * cleanup one more time ugh
    
    * added missing div
    
    ---------
    
    Co-authored-by: dashonaholt <dashona.n.holt@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,12 @@
 import '../css/Footer.css'
-import Instagram from "../assets/ph_instagram-logo-light.png"
-import Facebook from "../assets/ph_facebook-logo-light.png"
-import Twitter from "../assets/iconoir_twitter.png"
-import TikTok from "../assets/iconoir_tiktok.png"
+import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
-
     return (
         <div className="footer-container">
             <div className="footer-sub-container">
                 <div className="footer-social-media">
-                    {/* <p className="footer-title">Social Media</p> */}
-                    <div>
-                    <a href=""><img src={Instagram} alt="instagram icon" className="footer-icon"/></a>
-                <a href=""><img src={Facebook} alt="facebook icon" className="footer-icon"/></a>
-                <a href=""><img src={Twitter} alt="facebook icon" className="footer-icon"/></a>
-                <a href=""><img src={TikTok} alt="facebook icon" className="footer-icon"/></a>
-                    </div>
-                
+                <SocialMedia/>                
                 </div>
                 <div className="footer-links">
                     <h3>Made By:</h3><br />
@@ -26,11 +15,8 @@ export default function Footer() {
                     <a href="https://sisterchicago.github.io/portfolio/" target="_blank" className="website-link">Natalie's Portfolio</a>
                     <a href="https://super-stardust-4644df.netlify.app/about" target="_blank" className="website-link">Benjamin's Portfolio</a>
                 </div>
-            </div>
-            <script>
-
-            </script>
             <p className="copywrite">© MADSCRATCH G.O.A.T { new Date().getFullYear()}</p>
+            </div>
         </div>
     )
 }
