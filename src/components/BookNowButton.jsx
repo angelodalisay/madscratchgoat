c48373c6b2b8b811ebce7932290b2510d80136a0
Angelo Dalisay <dalisay.angelo@gmail.com>
2023-09-21 17:27:33 -0600

    Contact (#25)
    * work please
    
    * contact and bookingbutton
    
    * fixed css and cleanup
    
    * changes to codeowners
    
    * added tirzah as codeowner
    
    * soundcloud mix update (#17) (#20)
    
    * soundcloud mix update
    
    * Update SoundCloud.css
    
    adjusted gap from 66px to 5%. 
    
    Try to use percent whenever possible instead of exact pixel measurement.
    
    ---------
    
    Co-authored-by: Benjamin Vredevelt <59452308+bjv753@users.noreply.github.com>
    
    * creating a conflict
    
    * test
    
    * ignoring package.lock
    
    ---------
    
    Co-authored-by: Natalie Taylor <sisterchicago@gmail.com>
    Co-authored-by: Angelo Dalisay <adalisay@utah.gov>
    Co-authored-by: Benjamin Vredevelt <59452308+bjv753@users.noreply.github.com>

---
+++ src/components/BookNowButton.jsx
@@ -0,0 +1,11 @@
+import '../css/bookNowButton.css'
+
+export default function BookNowButton() {
+    return (
+        <div>
+            <a href='#contact'>
+                <button className='book-now'>BOOK NOW</button>
+            </a>
+        </div>
+    )
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com'
+import '../css/contact.css'
+
+function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [sentEmail, setSentEmail] = useState(false)
+
+    function submit() {
+        if (name && email && message) {
+            const serviceId = 'service_o7oc90r'
+            const templateId = 'template_495qvtm'
+            const userId = 'BVkqZR51ALDCg8xNH'
+            const templateParams = {
+                name,
+                email,
+                message
+            }
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error))
+
+            setName('')
+            setEmail('')
+            setMessage('')
+            setSentEmail(true)
+        } else {
+            alert ('Please fill in all fields.')
+        }
+    }
+
+    return (
+        <div id='contact'>
+            <div className='contact-us'>
+                <h1 className='header'>Contact us</h1>
+                <div className='text-input'>
+                    <label className='label'>Name</label>
+                    <input 
+                        type='text'
+                        className='name-input'
+                        placeholder='Name'
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                </div>
+                <div className='text-input'>
+                    <label className='label'>Email</label>
+                    <input
+                        type='email'
+                        className='email-input'
+                        placeholder='Email'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='message'>
+                    <label className='label'>Message</label>
+                    <textarea 
+                        placeholder='Your message here...'
+                        className='message-input'
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                    >
+                    </textarea>
+                </div>
+                {/* once email form has been completed and validated display Thank you/sent message*/}
+                <span className='sentEmail' onClick={sentEmail ? 'visible' : null}>Thank you for your message. We'll be in touch!</span>    
+                <button className='submit' onClick={submit}>Submit Message</button>
+
+            </div>   
+        </div>
+    )
+}
+export default Contact 
