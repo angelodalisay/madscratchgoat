fcd00f2cc84ff6c93a356bc92f9ca23c7834569b
Matthew Robinson <Mmrobinson1983@gmail.com>
2023-09-27 16:03:27 -0400

    added grayscale to Carousel img

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,23 +1,35 @@
-import '../css/SocialMedia.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
+import '../css/SocialMedia.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 export default function SocialMedia() {
-    library.add(fab, fas, far);
+    library.add(fab); // Only add the 'fab' (brands) icons
+
     return (
-        <div className="social-media-section">
-            <p className="title">Connect with us</p>
-            <div className="icon-container">
-                <a href="https://www.instagram.com/madscratchgoat/ "target="_blank"><FontAwesomeIcon icon="fa-brands fa-instagram" size="2xl"/></a>
-                <a href="https://www.facebook.com/madscratchgoat/ "target="_blank"><FontAwesomeIcon icon="fa-brands fa-facebook" size="2xl"/></a>
-                <a href="https://www.youtube.com/madscratchgoat/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-youtube" size="2xl"/></a>
-                <a href="https://www.tiktok.com/@madscratchgoat" target="_blank"><FontAwesomeIcon icon="fa-brands fa-tiktok" size="2xl"/></a>
-                <a href="https://soundcloud.com/mad-scratch-goat/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-soundcloud" size="2xl"/></a>
-                <a href="https://www.mixcloud.com/madscratchgoat/" target="_blank"><FontAwesomeIcon icon="fa-brands fa-mixcloud" size="2xl"/></a>
+        <div className="social-media-container">
+            <h2>Social Media</h2>
+            <div className="social-media-icons">
+                <div className="social-media-section">
+                    <p className="title">Connect with us</p>
+                    <div className="icon-container">
+                        <a href="https://www.instagram.com/madscratchgoat/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
+                        </a>
+                        <a href="https://www.facebook.com/madscratchgoat/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={['fab', 'facebook']} size="2x" />
+                        </a>
+                        <a href="https://twitter.com/madscratchgoat" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
+                        </a>
+                        <a href="https://www.tiktok.com/@madscratchgoat" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={['fab', 'tiktok']} size="2x" />
+                        </a>
+                    </div>
+                </div>
+                
             </div>
         </div>
-    )
+    );
 }
