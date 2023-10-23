50963a626f3c619379e8763b52720447373691d9
Natalie Taylor <sisterchicago@gmail.com>
2023-10-23 12:11:54 -0600

    carousel css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import '../css/Navbar.css'
+import '../css/navbar.css'
 import Logo from "../assets/MadScratchGoatLogo.png"
 
 export default function Navbar() {
@@ -8,7 +8,7 @@ export default function Navbar() {
             <a href="#" className="nav-item">EVENTS</a>
             <img src={Logo} alt="madscratchgoat logo" className="logo"/>
             <a href="#about-header" className="nav-item">ABOUT</a>
-            <a href="#" className="nav-item">CONTACT</a>
+            <a href="#contact" className="nav-item">CONTACT</a>
         </div>
     )
 }
