29f6e92838ac0b34854bb50923b633de82f81a1b
Angelo Dalisay <angelodalisay@Angelos-MacBook-Air.local>
2023-11-16 14:08:16 -0700

    adjusted to fix safari issue

--- src/components/BookNowButton.jsx
+++ src/components/BookNowButton.jsx
@@ -2,7 +2,7 @@ import '../css/bookNowButton.css'
 
 export default function BookNowButton() {
     return (
-        <div>
+        <div className="testtest">
             <a href='#contact'>
                 <button className='book-now'>BOOK NOW</button>
             </a>
